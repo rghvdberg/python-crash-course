187d1c5a4e8f9e3cc363b7e42a94c8ac5bf691ef
Rob van den Berg <rghvdberg@gmail.com>
2023-07-15 17:36:53 +0200

    strings - wip

---
+++ Strings/name.py
@@ -0,0 +1,27 @@
+# Changing Case in a String with Methods
+
+name = "ada lovelace"
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+
+# Combining or Concatenating Strings
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+# print(full_name)
+
+message = "Hello, " + full_name.title() +"!"
+print(message)
+
+
+# Adding Whitespace to Strings with Tabs or Newlines
+# tab = \t 
+# newline = \n 
+print("Hello,"+"\n\t" + first_name.title() + "\n\t\t" + last_name.title())
+
