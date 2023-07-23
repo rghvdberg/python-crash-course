fb2774b0b48c42084acd26d07eddd47b95c3ba1a
Rob van den Berg <rghvdberg@gmail.com>
2023-07-23 16:56:58 +0200

    bicycles.py

---
+++ Lists/bicycles.py
@@ -0,0 +1,11 @@
+bicycles = ['trek','cannondale','redline','specialized']
+print(bicycles)
+
+# Accessing Elements in a List
+print(bicycles[0])
+
+print(bicycles[0].title())
+
+# index -1 == last item in the List
+print(bicycles[-1])
+
