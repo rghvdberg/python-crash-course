ad66e254c23273759b32a6ca3448474682735878
Rob van den Berg <rghvdberg@gmail.com>
2023-08-12 22:35:30 +0200

    5-1 Conditional tests

---
+++ 05-if_statements/5-1_conditional_tests.py
@@ -0,0 +1,59 @@
+test = 1
+print(f"Conditional Test {test}")
+cat = "Mario"
+print("Is cat == 'Mario'? I predict True.")
+print(cat == "Mario")
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is cat == 'Luigi'? I predict False.")
+print(cat == "Luigi")
+
+number = 42
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is number == 42? I predict True.")
+print(number == 42)
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is number >= 10? I predict True.")
+print(number >= 10)
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is number < 42? I predict False.")
+print(number < 42)
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is number <= 42? I predict True.")
+print(number <= 42)
+
+colors = ["red", "green", "blue"]
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is 'red' in colors? I predict True.")
+print("red" in colors)
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is 'orange' in colors? I predict False.")
+print("orange" in colors)
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is 'blue' not in colors? I predict False.")
+print("blue" not in colors)
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Are 'red' and 'green' in colors? I predict True.")
+print("red" and "green" in colors)
+
+test = test + 1
+print(f"\nConditional Test {test}")
+print("Is 'red' or 'purple' in colors? I predict red.")
+print("red" or "green" in colors)
