6cf2da0649cf1aeffed3b291eceb26eec4c1c3da
Rob van den Berg <rghvdberg@gmail.com>
2023-08-12 22:36:08 +0200

    5-4 alien colors 2

---
+++ 05-if_statements/5-4_alien_colors_2.py
@@ -0,0 +1,14 @@
+alien_color = "blue"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+else:
+    print("You earned 10 points")
+
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("You earned 5 points!")
+else:
+    print("You earned 10 points")
